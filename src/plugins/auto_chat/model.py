e958f378a1e8fd822924358892337c53b4de8762
justundertaker <806792561@qq.com>
2022-01-22 02:55:54 +0800

    :bug:check的bug

--- src/plugins/auto_chat/model.py
+++ src/plugins/auto_chat/model.py
@@ -37,7 +37,7 @@ class Chat(object):
 
     def _check_nlp_config(self) -> bool:
         '''检查nlp配置'''
-        return (self._nlp_config.get("secretId") is not None) and (self._voice_config.get("secretKey") is not None)
+        return (self._nlp_config.get("secretId") is not None) and (self._nlp_config.get("secretKey") is not None)
 
     async def _chat_with_tencent(self, nickname: str, text: str) -> Optional[str]:
         '''使用腾讯云接口'''

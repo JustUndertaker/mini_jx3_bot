2371afa5139a8532a344b7ce689f3cd29bc932d0
justundertaker <806792561@qq.com>
2022-01-23 12:12:02 +0800

    :sparkles:智能闲聊插件

---
+++ src/plugins/chat/__init__.py
@@ -0,0 +1,25 @@
+from nonebot import export, on_message
+from nonebot.adapters.onebot.v11 import Bot
+from nonebot.adapters.onebot.v11.event import GroupMessageEvent
+from nonebot.adapters.onebot.v11.permission import GROUP
+from nonebot.rule import to_me
+
+from .model import chat
+
+Export = export()
+Export.plugin_name = "智能闲聊"
+Export.plugin_command = "@机器人 +你要说的话"
+Export.plugin_usage = "闲聊功能，有一句没一句"
+Export.default_status = True
+
+
+chat_query = on_message(rule=to_me(), permission=GROUP, priority=9, block=True)
+
+
+@chat_query.handle()
+async def _(bot: Bot, event: GroupMessageEvent):
+    '''智能闲聊'''
+    nickname = list(bot.config.nickname)[0]
+    message = event.get_plaintext()
+    msg = await chat.chat(nickname, message)
+    await chat_query.finish(msg)

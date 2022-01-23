f50257fee338879f22a4591c4c7a26b1ccbec873
justundertaker <806792561@qq.com>
2022-01-23 12:18:29 +0800

    :sparkles:舔狗日记插件

---
+++ src/plugins/tiangou/__init__.py
@@ -0,0 +1,22 @@
+from nonebot import export, on_regex
+from nonebot.adapters.onebot.v11.event import GroupMessageEvent
+from nonebot.adapters.onebot.v11.permission import GROUP
+
+from .data_source import get_tiangou
+
+Export = export()
+Export.plugin_name = "舔狗日记"
+Export.plugin_command = "舔狗|日记|舔狗日记"
+Export.plugin_usage = "发送一条舔狗日记"
+Export.default_status = True
+
+
+tiangou_regex = r"(^舔狗$)|(^日记$)|(^舔狗日记$)"
+tiangou = on_regex(pattern=tiangou_regex, permission=GROUP, priority=5, block=True)
+
+
+@tiangou.handle()
+async def _(event: GroupMessageEvent):
+    '''舔狗日记'''
+    msg = await get_tiangou()
+    await tiangou.finish(msg)
---
+++ src/plugins/tiangou/data_source.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from typing import Optional
+
+from httpx import AsyncClient
+from src.utils.config import config
+from src.utils.log import logger
+
+
+async def get_tiangou() -> Optional[str]:
+    '''
+    :说明
+        获取一条舔狗日记
+
+    :返回
+        * `str`：获取内容
+        * `None`：转换出错
+    '''
+    jx3_url: str = config.jx3api['jx3_url']
+    url = f"{jx3_url}/share/random"
+
+    async with AsyncClient() as client:
+        try:
+            req_url = await client.get(url=url)
+            req = req_url.json()
+            if req['code'] == 200:
+                data = req['data']
+                text = data['text']
+                log = f"请求日记成功：{text}"
+                logger.debug(log)
+                date_now = datetime.now()
+                date_str = date_now.strftime('%Y年%m月%d日')
+                req_text = date_str+"\n"+text
+                return req_text
+            else:
+                log = f'请求日记出错：{req["msg"]}'
+                logger.debug(log)
+                return None
+        except Exception as e:
+            log = f'请求链接失败，原因：{str(e)}'
+            logger.error(log)
+            return None

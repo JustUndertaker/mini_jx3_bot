4f5bf6c625fc2d0ea01321421768d6cc528db823
justundertaker <806792561@qq.com>
2022-06-26 11:52:08 +0800

    :recycle:search_record重构

--- src/modules/search_record.py
+++ src/modules/search_record.py
@@ -21,21 +21,44 @@ class SearchRecord(Model):
         table_description = "记录查询次数"
 
     @classmethod
-    async def get_search_time(cls, group_id: int, app_name: str) -> bool:
-        '''获取上次查询记录时间'''
+    async def get_search_time(cls, group_id: int, app_name: str) -> int:
+        '''
+        说明:
+            获取上次查询记录时间
+
+        参数:
+            * `group_id`：群号
+            * `app_name`：app名称
+
+        返回:
+            * `int`：上次查询时间戳
+        '''
         record, _ = await SearchRecord.get_or_create(group_id=group_id, app_name=app_name)
         return record.last_time
 
     @classmethod
     async def use_search(cls, group_id: int, app_name: str):
-        '''使用一次查询'''
+        '''
+        说明:
+            使用一次查询
+
+        参数:
+            * `group_id`：群号
+            * `app_name`：app名称
+        '''
         record, _ = await SearchRecord.get_or_create(group_id=group_id, app_name=app_name)
         time_now = int(time.time())
         record.last_time = time_now
         record.count += 1
-        await record.save(update_fields=["last_time", "count"])
+        await record.save()
 
     @classmethod
     async def delete_group(cls, group_id: int):
-        '''删除群'''
+        '''
+        说明:
+            删除一个群所有记录，注销时使用
+
+        参数:
+            * `group_id`：群号
+        '''
         await cls.filter(group_id=group_id).delete()

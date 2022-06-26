c217da95e74c5ac34fac5bb5060eecf1a5beff1d
justundertaker <806792561@qq.com>
2022-06-26 12:06:59 +0800

    :recycle:user_info重构

--- src/modules/user_info.py
+++ src/modules/user_info.py
@@ -32,10 +32,17 @@ class UserInfo(Model):
 
     @classmethod
     async def user_init(cls, user_id: int, group_id: int, user_name: str):
-        '''用户注册，刷新昵称'''
+        '''
+        说明:
+            注册一个用户并刷新昵称
+
+        参数:
+            * `group_id`：群号
+            * `user_name`：用户昵称
+        '''
         record, _ = await UserInfo.get_or_create(user_id=user_id, group_id=group_id)
         record.user_name = user_name
-        await record.save(update_fields=["user_name"])
+        await record.save()
 
     @classmethod
     async def sign_in(cls,
@@ -48,25 +55,25 @@ class UserInfo(Model):
                       lucky_gold: int
                       ) -> Dict[str, int]:
         '''
-        :说明
+        说明:
             设置签到
 
-        :参数
-            * user_id：用户QQ
-            * group_id：QQ群号
-            * lucky_min：最小运势
-            * lucky_max：最大运势
-            * friendly_add：签到获取的好友度系数
-            * gold_base：金币底薪
-            * lucky_gold：幸运值影响因子
+        参数:
+            * `user_id`：用户QQ
+            * `group_id`：QQ群号
+            * `lucky_min`：最小运势
+            * `lucky_max`：最大运势
+            * `friendly_add`：签到获取的好友度系数
+            * `gold_base`：金币底薪
+            * `lucky_gold`：幸运值影响因子
 
-        :返回
-            * dict[str,int]：返回数据字典
-            * ``"today_lucky"``：今日运势
-            * ``"today_gold"``：今日金币
-            * ``"all_gold"``：总金币
-            * ``"all_friendly"``：好友度
-            * ``"sign_times"``：签到次数
+        返回:
+            * `dict[str,int]`：返回数据字典
+                * `today_lucky`：今日运势
+                * `today_gold`：今日金币
+                * `all_gold`：总金币
+                * `all_friendly`：好友度
+                * `sign_times`：签到次数
         '''
         record, _ = await UserInfo.get_or_create(user_id=user_id, group_id=group_id)
         # 设置签到日期
@@ -84,23 +91,52 @@ class UserInfo(Model):
         record.friendly += today_friendy
         # 累计签到次数
         record.sign_times += 1
-        data = {
+        await record.save()
+        return {
             "today_lucky": today_lucky,
             "today_gold": today_gold,
             "all_gold": all_gold,
             "all_friendly": record.friendly,
             "sign_times": record.sign_times
         }
-        await record.save(update_fields=["last_sign", "lucky", "gold", "friendly", "sign_times"])
-        return data
 
     @classmethod
     async def get_last_sign(cls, user_id: int, group_id: int) -> date:
-        '''获取上次签到时间'''
+        '''
+        说明:
+            获取上次签到时间
+
+        参数:
+            * `user_id`：用户qq
+            * `group_id`：群号
+
+        返回:
+            * `date`：签到日期
+        '''
         record, _ = await UserInfo.get_or_create(user_id=user_id, group_id=group_id)
         return record.last_sign
 
     @classmethod
     async def delete_group(cls, group_id: int):
-        '''删除群'''
+        '''
+        说明:
+            删除一个群的所有用户记录，注销时使用
+
+        参数:
+            * `group_id`：群号
+        '''
         await cls.filter(group_id=group_id).delete()
+
+    @classmethod
+    async def delete_user(cls, user_id: int, group_id: int):
+        '''
+        说明:
+            删除一条用户记录，用户退群时使用
+
+        参数:
+            * `user_id`：用户qq
+            * `group_id`：群号
+        '''
+        record = await cls.get_or_none(user_id=user_id, group_id=group_id)
+        if record:
+            await record.delete()

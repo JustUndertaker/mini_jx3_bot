08b8530628df220059e28455e9f3bd40a391ac48
justundertaker <806792561@qq.com>
2022-06-28 13:11:37 +0800

    :pencil:图片处理类注释

--- src/managers/group_manager/model.py
+++ src/managers/group_manager/model.py
@@ -4,19 +4,34 @@ from httpx import AsyncClient
 from nonebot.utils import run_sync
 
 
-class ImageHandler(object):
-    '''图片处理类，将同步储存更换为异步执行'''
+class ImageHandler:
+    '''
+    图片处理类，将同步储存更换为异步执行
+    '''
 
     @classmethod
     @run_sync
     def _save_image(cls, file_name: Path, data: bytes):
-        '''储存图片'''
+        '''
+        说明:
+            将数据保存到本地
+        '''
         with open(file_name, mode='wb') as f:
             f.write(data)
 
     @classmethod
     async def save_image(cls, file_name: Path, url: str) -> bool:
-        '''储存图片'''
+        '''
+        说明:
+            从url获取图片并保存到本地
+
+        参数:
+            * `file_name`：保存的图片路径
+            * `url`：图片url地址
+
+        返回:
+            * `bool`：保存是否成功
+        '''
         async with AsyncClient() as client:
             try:
                 req = await client.get(url=url)
@@ -27,8 +42,17 @@ class ImageHandler(object):
 
     @classmethod
     @run_sync
-    def load_image(cls, file_name: str) -> bytes:
-        '''读取图片'''
+    def load_image(cls, file_name: Path) -> bytes:
+        '''
+        说明:
+            从本地读取图片数据
+
+        参数:
+            * `file_name`：图片路径
+
+        返回:
+            * `bytes`：图片数据
+        '''
         with open(file_name, 'rb') as f:
             data = f.read()
         return data

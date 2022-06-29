74cc20d9e00bcb8aa476b7d7f65ef0a861d62afb
justundertaker <806792561@qq.com>
2022-06-29 20:42:54 +0800

    :fire:删除不必要的文件

--- bot.py
+++ bot.py
@@ -5,11 +5,8 @@ import nonebot
 from nonebot.adapters.onebot.v11 import Adapter
 
 from src.utils.database import database_init
-from src.utils.moinkeypath import monkeypatch
 from src.utils.scheduler import start_scheduler
 
-# 猴子补丁，针对windows平台，更换事件循环
-monkeypatch()
 nonebot.init()
 app = nonebot.get_asgi()
 

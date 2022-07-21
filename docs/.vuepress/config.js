e9d92dea0d0fbb1d742581ac78ad1cbef2bc1b9d
justundertaker <806792561@qq.com>
2022-07-21 17:49:20 +0800

    :pencil:增加文档测试

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  title: '团子机器人',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }],
+  ],
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  themeConfig: {
+    logo: 'logo.png',
+    sidebarDepth: 2,
+    lastUpdated: 'Last Updated',
+    nav: [
+      { text: '用户文档', link: '/guide/' },
+      { text: '开发手册', link: '/development/' },
+      { text: 'Github', link: 'https://github.com/JustUndertaker/mini_jx3_bot' }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: '用户文档',
+          sidebarDepth: 2,
+          children: [
+            ['/guide/', '部署机器人'],
+            ['/guide/config', '设置配置'],
+            ['/guide/setting', '启动机器人'],
+          ]
+        }
+      ],
+      '/development/': [
+        {
+          title: '开发手册',
+        }
+      ]
+    }
+  },
+  description: '一个女生自用的剑网三机器人，内置了部分插件，欢迎二次开发！'
+}
---
+++ docs/README.md
@@ -0,0 +1,12 @@
+---
+home: true
+heroImage: /logo.gif
+actionText: 查看文档
+actionLink: /guide/
+features:
+- title: 部署简单
+  details: 操作步骤没几个。
+- title: 开发简单
+  details: 开发起来也没那么难。
+footer: MIT Licensed | Copyright © JustUndertaker
+---

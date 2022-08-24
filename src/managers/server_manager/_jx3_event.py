df9befaba1d796d49fb1b44bc4aa9233f6653342
justundertaker <806792561@qq.com>
2022-08-24 09:28:08 +0800

    :bug:ws扶摇通知修复

--- src/managers/server_manager/_jx3_event.py
+++ src/managers/server_manager/_jx3_event.py
@@ -311,7 +311,7 @@ class FuyaoNamedEvent(RecvEvent):
 
     __event__ = "WsRecv.FuyaoNamed"
     message_type = "FuyaoNamed"
-    names: list[str]
+    name: list[str]
     """点名角色组"""
     time: str
     """点名时间"""
@@ -323,13 +323,13 @@ class FuyaoNamedEvent(RecvEvent):
 
     @property
     def log(self) -> str:
-        name = ",".join(self.names)
+        name = ",".join(self.name)
         log = f"扶摇点名事件：[{self.server}]的扶摇点名了，玩家[{name}] 。"
         return log
 
     @overrides(RecvEvent)
     def get_message(self) -> Message:
-        name = ",".join(self.names)
+        name = ",".join(self.name)
         return Message(f"[扶摇监控] 时间：{self.time}\n唐文羽点名了[{name}]。")
 
 
